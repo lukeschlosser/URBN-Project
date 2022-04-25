904a6e4fdb24ce7116651ab8549e7d61637379e5
lukeschlosser <jonlukeschlosser@gmail.com>
2022-04-25 16:55:10 -0400

    server runs, backend "complete" need to test getWeatherByPostalCode in postman

--- SpringBoot/wearther/src/main/java/com/URBNProject/wearther/controller/WeatherController.java
+++ SpringBoot/wearther/src/main/java/com/URBNProject/wearther/controller/WeatherController.java
@@ -1,11 +1,27 @@
 package com.URBNProject.wearther.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.URBNProject.wearther.model.Forecast;
+import com.URBNProject.wearther.services.WeatherService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@CrossOrigin
 public class WeatherController {
+
+    String API_BASE_URL = "http://dataservice.accuweather.com";
+
+    private WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService){
+        this.weatherService = weatherService;
+    }
+
+    @RequestMapping(path = "/weather/{postalCode}", method = RequestMethod.GET)
+    public List<Forecast> getWeatherByPostalCode(@PathVariable String postalCode) {
+        return weatherService.getWeatherByPostalCode(postalCode);
+    }
 
 
 }
--- SpringBoot/wearther/src/main/java/com/URBNProject/wearther/services/WeatherService.java
+++ SpringBoot/wearther/src/main/java/com/URBNProject/wearther/services/WeatherService.java
@@ -5,10 +5,17 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@Component
 public class WeatherService {
 
     private RestTemplate restTemplate = new RestTemplate();
@@ -18,7 +25,7 @@ public class WeatherService {
     private String searchByKey = "/currentconditions/v1/";
     private String API_KEY = "?apikey=ycWGVPxLK73Vg9GhLGeLQnjHaRxX5vBX";
 
-    // FULL GET URL for currentconditions http://dataservice.accuweather.com/currentconditions/v1/{key goes here}?apikey=ycWGVPxLK73Vg9GhLGeLQnjHaRxX5vBX
+    // FULL GET URL for current conditions http://dataservice.accuweather.com/currentconditions/v1/{key goes here}?apikey=ycWGVPxLK73Vg9GhLGeLQnjHaRxX5vBX
 
     public WeatherService(){}
 
@@ -31,13 +38,28 @@ public class WeatherService {
         return new HttpEntity<>(headers);
     }
 
-    public Forecast getWeatherByPostalCode(String postalCode) {
-        return null;
+    public List<Forecast> getWeatherByPostalCode(String postalCode) {
+        List<Forecast> forecastList = new ArrayList<>();
+        String key = fetchKeyFromPostalCode(postalCode);
+        String forecastJsonString = null;
+
+        try {
+
+            String path = API_BASE_URL + searchByKey + key + API_KEY;
+
+            ResponseEntity<String> response = restTemplate.exchange(path, HttpMethod.GET, makeHeaders(), String.class);
+            forecastJsonString = response.getBody();
+
+        } catch(RestClientResponseException | ResourceAccessException e){
+            System.out.println(e.getMessage());
+        }
+
+        return forecastJsonString(forecastJsonString, forecastList);
     }
 
     //retrieve a key from given valid postal code
     //TODO test in postman once controller setup
-    public String fetchKeyFromPostalCode(int postalCode){
+    public String fetchKeyFromPostalCode(String postalCode){
         String jsonString = null;
         String key = "";
 
@@ -61,8 +83,65 @@ public class WeatherService {
         int indexOfKey = jsonString.indexOf(key);
         int beginIndexOfValue = indexOfKey + key.length() + offset;
         String[] split = jsonString.substring(beginIndexOfValue).split(",");
-        String isolatedKey = split[0];
+        String isolatedKey = split[1];
         return isolatedKey;
+    }
+
+    public String fetchForecastDetails(String pathKey, String forecastJsonString, Integer offset){
+
+        int indexOfPathKey = forecastJsonString.indexOf(pathKey);
+        int indexStart = indexOfPathKey + pathKey.length() + offset;
+
+        String[] split = forecastJsonString.substring(indexStart).split("\"");
+        String isolatedPath = split[0];
+
+        return isolatedPath;
+    }
+
+    public List<String> extractWeatherInformation(String forecastJsonString){
+
+        List<String> listOfForecastInfo = new ArrayList<>();
+
+        String key = fetchKeyFromJsonString("\"Key\"", forecastJsonString, 1);
+        listOfForecastInfo.add(key);
+
+        String weatherText = fetchForecastDetails("\"WeatherText\"", forecastJsonString, 1);
+        listOfForecastInfo.add(weatherText);
+
+        String temperatureC = fetchForecastDetails("\"Value\"", forecastJsonString, 1);
+        listOfForecastInfo.add(temperatureC);
+        //TODO figure out if going to convert or grab the second "Value"
+        String temperatureF = fetchForecastDetails("\"Value\"", forecastJsonString, 1);
+        listOfForecastInfo.add(temperatureF);
+
+        String hasPrecipitation = fetchForecastDetails("\"HasPrecipitation\"", forecastJsonString, 1);
+        listOfForecastInfo.add(hasPrecipitation);
+
+        String link = fetchForecastDetails("\"Link\"", forecastJsonString, 1);
+        listOfForecastInfo.add(link);
+
+        return listOfForecastInfo;
+    }
+
+    public List<Forecast> forecastJsonString(String listForecastJsonString, List<Forecast> listForecast){
+
+        while(listForecastJsonString.contains("\"LocalObservationDateTime\"")){
+            Forecast forecast = new Forecast();
+            List<String> listOfForecast = new ArrayList<>();
+
+            listOfForecast = extractWeatherInformation(listForecastJsonString);
+
+            forecast.setKey(listOfForecast.get(0));
+            forecast.setWeatherText(listOfForecast.get(1));
+            forecast.setTemperatureC(listOfForecast.get(2));
+            forecast.setTemperatureF(listOfForecast.get(3));
+            forecast.setHasPrecipitation(listOfForecast.get(4).equalsIgnoreCase("true"));
+            forecast.setLink(listOfForecast.get(5));
+
+            listForecast.add(forecast);
+        }
+
+        return listForecast;
     }
 
 }
